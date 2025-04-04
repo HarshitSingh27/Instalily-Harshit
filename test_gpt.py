92b3d8e42a506a6e6314ebcdf5a46d305876c1ec
Harshit Singh <54538211+HarshitSingh27@users.noreply.github.com>
2025-04-04 15:49:57 -0500

    Update

--- test_gpt.py
+++ test_gpt.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  api_key=os.getenv("OPENAI_API_KEY"))
 )
 
 completion = client.chat.completions.create(
